1f39b9817a24ca27e4a2f2d2e528759dfdfd5ac0
Cebastion <kdimon2006@gmail.com>
2024-08-03 22:53:04 +0300

    new commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,32 @@
 'use client'
 import Link from 'next/link'
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import style from './Header.module.scss'
 
 const Header: FC = () => {
   const [showMenu, setShowMenu] = useState(false)
+  const [widthWindow, setWidthWindow] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setWidthWindow(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
 
   return (
     <header className={style.header}>
       <div className={style.header__container}>
         <ul className={style.header__menu}>
           <li>
-            <Link href='/'>Главная</Link>
+            <Link href='/' style={{ display: widthWindow > 540 ? 'block' : 'none' }}>Главная</Link>
           </li>
           <li>
             <Link href='/forecast'>Прогноз по годам</Link>
@@ -19,16 +34,33 @@ const Header: FC = () => {
           <li>
             <Link href='/compatibility'>Совместимость</Link>
           </li>
-          <li>
+          <li style={{ display: widthWindow > 768 ? 'block' : 'none' }}>
             <Link href='/blog'>Блог</Link>
           </li>
-          <li>
+          <li style={{ display: widthWindow > 768 ? 'block' : 'none' }}>
             <Link href='/account'>Личный кабинет</Link>
           </li>
+          {showMenu && (
+            <>
+              <div className={style.blur}></div>
+              <ul className={style.header__menu__mobile}>
+                <h2>Меню</h2>
+                <li style={{ display: widthWindow >= 540 ? 'none' : 'block' }}>
+                  <Link href='/'>Главная</Link>
+                </li>
+                <li>
+                  <Link href='/blog'>Блог</Link>
+                </li>
+                <li>
+                  <Link href='/account'>Личный кабинет</Link>
+                </li>
+              </ul>
+            </>
+          )}
+          <div className={showMenu ? `${style.burger} ${style.active}` : style.burger} onClick={toggleMenu}>
+            <span></span>
+          </div>
         </ul>
-        <div className="burger">
-          <span></span>
-        </div>
       </div>
     </header>
   )
